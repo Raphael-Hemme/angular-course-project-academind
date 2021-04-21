65cd5cd1fd7c6007faefedc62632418d470b266f
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2021-04-21 14:29:07 +0200

    Add improvised navigation with *ngIf instead of actual routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  navigationChoice: string = ''
   
+  navigate(choice) {
+    this.navigationChoice = choice;
+  }
 }
