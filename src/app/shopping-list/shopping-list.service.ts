0c5074a39176ed4ecdcb02ffd82145967ce97413
Raphael-Hemme <55955538+Raphael-Hemme@users.noreply.github.com>
2021-04-28 15:56:14 +0200

    Transition from eventemitters to rxjs subject subscriptions

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 3),
@@ -14,14 +15,14 @@ export class ShoppingListService {
     }
     addIngredient(newIngredient: Ingredient) {
         this.ingredients.push(newIngredient)
-        this.ingredientsChanged.emit(this.ingredients.slice())
+        this.ingredientsChanged.next(this.ingredients.slice())
     }
     addIngredients(ingredients: Ingredient[]) {
     /*  for (let ingredient of ingredients) {
             this.addIngredient(ingredient);
         } */
-        this.ingredients.push(...ingredients, );
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredients.push(...ingredients);
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
 }
